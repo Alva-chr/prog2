d940a2e33ecad01c065caeebcf65ae78e90b55c1
Alva-chr <alvachristensson03@gmail.com>
2025-05-12 12:11:46 +0200

    Solved MA4 1.1

--- Inlämningsuppgifter/MA4/MA4_files/MA4_1_1.py
+++ Inlämningsuppgifter/MA4/MA4_files/MA4_1_1.py
@@ -10,10 +10,41 @@ reviewer = ""
 
 import random as r
 import matplotlib.pyplot as plt 
+from math import sqrt, pi
 
 def approximate_pi(n):
-    # Write your code here
-    return
+    x_c = []
+    y_c = []
+    n_c = 0
+    
+    x_s = []
+    y_s = []
+    n_s = 0
+
+    for i in range(n):
+        x = r.uniform(-1,1)
+        y = r.uniform(-1,1)
+
+        distance = sqrt(x*x+y*y)
+
+        if distance < 1:
+            x_c.append(x)
+            y_c.append(y)
+            n_c +=1
+
+        else:
+            x_s.append(x)
+            y_s.append(y)
+            n_s +=1
+    plt.figure(figsize=(10,10))
+    plt.scatter(x_c, y_c, color = "red")
+    plt.scatter(x_s,y_s, color="blue")
+    plt.savefig(f"circleplot{n}.png")
+
+    print(f"Number of points in circle: {n_c}")
+    print(f"Pi is approx: {4*n_c/(n)}" )
+    print(f"Pi is given as: {pi}")
+    return 4/(n)
     
 def main():
     dots = [1000, 10000, 100000]
